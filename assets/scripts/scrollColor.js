9311f145dc30735aa27371576e1bc4a71ad5bcce
Havas Health Plus <tyler.hancock@havas.com>
2018-06-20 16:00:05 -0400

    init commit

---
+++ assets/scripts/scrollColor.js
@@ -0,0 +1,28 @@
+$(window).on("scroll touchmove", function() {
+
+  if ($(document).scrollTop() >= $("#home").position().top) {
+
+    $('body').css('background', $("#home").attr("data-color"));
+
+  };
+
+  if ($(document).scrollTop() > $("#biotech").position().top) {
+
+    $('body').css('background', $("#biotech").attr("data-color"))
+
+  };
+
+  if ($(document).scrollTop() > $("#break").position().top) {
+
+    $('body').css('background', $("#break").attr("data-color"))
+
+  };
+
+  if ($(document).scrollTop() > $("#manu").position().top) {
+
+    $('body').css('background', $("#manu").attr("data-color"))
+
+  };
+
+
+});
