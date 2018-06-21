75786764ebc2dc383f7c41a8a9eb39283b8aa456
Havas Health Plus <tyler.hancock@havas.com>
2018-06-21 17:43:20 -0400

    adding new arrows

--- assets/scripts/scrollColor.js
+++ assets/scripts/scrollColor.js
@@ -24,6 +24,12 @@ $(window).on("scroll touchmove", function() {
 
   };
 
+  if ($(document).scrollTop() > $("#service_break").position().top) {
+
+    $('body').css('background', $("#service_break").attr("data-color"))
+
+  };
+
   if ($(document).scrollTop() > $("#services").position().top) {
 
     $('body').css('background', $("#services").attr("data-color"))
